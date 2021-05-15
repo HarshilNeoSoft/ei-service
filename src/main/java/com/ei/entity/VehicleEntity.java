c4a5e9c71627cfc679ff6a4928a74497909f2536
Harshil Modi <harshil.modi@neosoftmail.com>
2021-05-15 11:43:45 +0530

    validation for vehicle entity

--- src/main/java/com/ei/entity/VehicleEntity.java
+++ src/main/java/com/ei/entity/VehicleEntity.java
@@ -3,6 +3,8 @@ package com.ei.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
@@ -13,8 +15,14 @@ public class VehicleEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @NotNull(message = "Name should not be null")
+    @NotEmpty(message = "Name should not be empty")
     private String name;
+    @NotNull(message = "Fuel type should not be null")
+    @NotEmpty(message = "Fuel type should not be empty")
     private String fuelType;
+    @NotNull(message = "Max speed should not be null")
+    @NotEmpty(message = "Max speed should not be empty")
     private String maxSpeed;
 
     @OneToOne
